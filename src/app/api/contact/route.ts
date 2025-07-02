d62b0ce81c56424836f9067447731d67ca2bee78
Aryan Bansal <aryan.bansal@contentstack.com>
2025-07-02 14:30:10 +0530

    add log in contact api

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,6 +1,7 @@
 
  export async function POST(request: Request) {
   const data = request.body;
+  console.log("data from api route contact");
   console.log(data);
   const  uid = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
   return new Response('Hello, Next.js!', {
