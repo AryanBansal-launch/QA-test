958f633e8e6fa23fdd9ec994bc9d6eeec604c084
Aryan Bansal <aryan.bansal@contentstack.com>
2026-05-16 13:56:53 +0530

    Modify homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
+      <h1>This is simple Nextjs Test application</h1>
       Home Page
       <Link href="/about">About</Link>
       <Link href="/contact">Contact</Link>
