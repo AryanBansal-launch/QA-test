b5a263432f031636bf35cd44d03ae6b2488943a2
Aryan Bansal <aryan.bansal@contentstack.com>
2026-05-28 14:15:37 +0530

    test commit

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 const page = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      About us Page
+      About us Page from app
       <Link href="/">Home</Link>
       <Link href="/contact">Contact</Link>
     </div>
