3eb83bcea354b9d2d2b2059e4e01a697864ad684
Aryan Bansal <aryan.bansal@contentstack.com>
2026-06-09 18:42:41 +0530

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/wf/:path*",
+        destination: "https://r.eu-north-1.awstrack.me/wf/:path*",
+      },
+    ];
+  },
   async headers() {
     return [
       {
-        source: "/about", 
+        source: "/about",
         headers: [
           {
             key: "Cache-Control",
