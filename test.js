7fa6a4c02c42352fd8cf70b19fbefe7b4ed06778
Aryan Bansal <aryan.bansal@contentstack.com>
2026-06-09 18:37:08 +0530

    add test smail script

--- test.js
+++ test.js
@@ -3,24 +3,24 @@ const { SESClient, SendEmailCommand } = require("@aws-sdk/client-ses");
 const client = new SESClient({ region: "eu-north-1" });
 
 const command = new SendEmailCommand({
-  Source: "aryan@bansalapp.digital",
-  Destination: {
-    ToAddresses: ["bansalaryan2929@gmail.com"],
-  },
-  Message: {
-    Subject: { Data: "Click tracking test" },
-    Body: {
-      Html: {
-        Data: `
-          <h1>Click tracking test</h1>
-          <a href="https://bansalapp.digital">Link 1 - Homepage</a><br/><br/>
-          <a href="https://marketplace.bansalapp.digital">Link 2 - Marketplace</a>
-        `,
+    Source: "aryan@bansalapp.digital",
+    Destination: {
+      ToAddresses: ["bansalaryan2929@gmail.com"],
+    },
+    Message: {
+      Subject: { Data: "Click tracking test v3" },
+      Body: {
+        Html: {
+          Data: `
+            <h1>Click tracking test</h1>
+            <a href="https://bansalapp.digital">Link 1</a><br/><br/>
+            <a href="https://marketplace.bansalapp.digital">Link 2</a>
+          `,
+        },
       },
     },
-  },
-  ConfigurationSetName: "bansalapp-config",
-});
+    ConfigurationSetName: "bansalapp-config",
+  });
 
 client.send(command)
   .then((res) => console.log("Email sent! Message ID:", res.MessageId))
